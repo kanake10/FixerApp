0ddce7fca7381211b3d82e41a8347457f45a55e9
Kanake <10kanake@gmail.com>
2025-03-06 07:20:05 +0300

    free tier api key

--- core/src/main/java/com/example/core/Constants.kt
+++ core/src/main/java/com/example/core/Constants.kt
@@ -3,7 +3,7 @@ package com.example.core
 object Constants {
     //hide api key
     const val BASE_URL = "https://data.fixer.io/api/"
-    const val API_KEY = "818db86f8083d887073a235d2c0ed6c0"
+    const val API_KEY = "5dfa9204620b5076a02960a52bfecb19"
     const val DB_NAME = "currency.db"
     const val GET_EXCHANGE_RATES = "$BASE_URL/latest"
     const val GET_CURRENCY_SYMBOLS = "$BASE_URL/symbols"
