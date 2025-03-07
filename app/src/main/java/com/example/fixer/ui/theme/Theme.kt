913ecb0dd6abf957052c0a70e8dbd83c4b78188b
Kanake <10kanake@gmail.com>
2025-03-07 22:05:53 +0300

    disable dynamic theming

--- app/src/main/java/com/example/fixer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/fixer/ui/theme/Theme.kt
@@ -26,15 +26,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -51,7 +47,7 @@ private val LightColorScheme = lightColorScheme(
 fun FixerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
